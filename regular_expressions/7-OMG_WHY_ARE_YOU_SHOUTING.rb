73522261e4083d697acf666d9bb9b371944657f7
root <s.otieno@alustudent.com>
2024-05-16 02:14:10 -0700

    regular expressions

--- regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-
-input_string = ARGV[0] # Obtain the first command-line argument
-
-matches = input_string.scan(/[\A-Z]/)
-result =  matches.join
-puts matches
+puts ARGV[0].scan(/[A-Z]/).join

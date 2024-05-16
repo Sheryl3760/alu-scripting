5c1ed78b1f465e6ff44fbe3daa9dc4d93b0a6933
root <s.otieno@alustudent.com>
2024-05-16 01:57:13 -0700

    alu scripting

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-
-input_string = ARGV[0] # Obtain the first command-line argument
-
-matches = input_string.match(/hbt{2,5}n/) # Find the first occurrence that matches the pattern
-
-puts matches
+puts ARGV[0].scan(/hbt{2,5}n/).join
